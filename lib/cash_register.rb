d04de1f26d73f997c66075fb1b9d4e093e070822
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-17 15:51:35 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,4 +10,10 @@ class CashRegister
     self.total += (price * quantity)
   end
 
+  def apply_discount
+    if self.discount != 0
+      self.total -= (self.total * self.discount) / 100
+    end
+  end
+
 end
