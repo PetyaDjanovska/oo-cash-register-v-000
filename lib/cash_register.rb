c5d1274d38dd37fd21526298a156fd028980e8f1
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-17 15:45:31 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,7 +6,7 @@ class CashRegister
     self.discount = employee_discount
   end
 
-  def add_item(title, price, quantity = 0)
+  def add_item(title, price, quantity = 1)
     self.total += price
   end
 
