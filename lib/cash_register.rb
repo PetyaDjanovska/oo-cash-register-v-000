0c698e97855924f6b16b1131d87a86cf44d8ba3d
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-17 15:40:06 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
 
   def initialize(employee_discount = 0)
     @total = 0
-    # self.discount(employee_discount)
+    @discount = employee_discount
   end
 
   # def discount(employee_discount)
